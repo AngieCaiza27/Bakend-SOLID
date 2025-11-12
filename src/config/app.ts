89ce5368fc0ac447be0e280768d23588e38363ad
Angie Nohemi Caiza Valle <119009870+AngieCaiza27@users.noreply.github.com>
2025-11-12 17:59:10 -0500

    Version 1

---
+++ src/config/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+export function createApp() {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+
+  app.get("/", (req, res) => {
+    res.json({ message: "API funcionando correctamente" });
+  });
+
+  return app;
+}
